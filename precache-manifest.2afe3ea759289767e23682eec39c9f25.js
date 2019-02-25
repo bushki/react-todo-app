c96d46b23d816ab7ab26dae040e6a9238be685ce
Luis M <melgar.luis@gmail.com>
2019-02-24 20:51:39 -0800

    Updates

---
+++ precache-manifest.2afe3ea759289767e23682eec39c9f25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0945be15e27dd30a16be",
+    "url": "/react-todo-app/static/js/runtime~main.0945be15.js"
+  },
+  {
+    "revision": "89e0f7b713f46b82407c",
+    "url": "/react-todo-app/static/js/main.89e0f7b7.chunk.js"
+  },
+  {
+    "revision": "23508a2d5402f5145dbd",
+    "url": "/react-todo-app/static/js/2.23508a2d.chunk.js"
+  },
+  {
+    "revision": "89e0f7b713f46b82407c",
+    "url": "/react-todo-app/static/css/main.b9ef003e.chunk.css"
+  },
+  {
+    "revision": "71adf422187bf1da1949b6cbb68fe95c",
+    "url": "/react-todo-app/index.html"
+  }
+];
